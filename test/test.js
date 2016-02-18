2dbbec986c5651bc3c7ae5b4df3bebdbac5523d1
MSturges <Sturges3232@gmail.com>
2016-02-17 17:46:01 -0700

    Solution

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
-var code = require('../main.js')
-var expect = require('chai').expect
+var code = require('../main.js');
+var expect = require('chai').expect;
 
-describe('leapYear', function() {
-  it('can calculate leapness of 2016', function() {
-    expect(code.leapYear(2016)).to.equal(true)
+describe("leapYear", function() {
+
+  it("returns true for 2016", function() {
+    expect(code.leapYear(2016)).to.equal(true);
+  });
+  it("returns false for 2015", function () {
+    expect(code.leapYear(2015)).to.equal(false);
   })
-})
+  it("returns true for 1600", function () {
+    expect(code.leapYear(1600)).to.equal(true);
+  })
+  it("returns false for 1700", function () {
+    expect(code.leapYear(1700)).to.equal(false);
+  })
+});
